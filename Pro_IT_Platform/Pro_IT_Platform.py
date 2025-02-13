cd05a06cd5b057a64b328bb5c75268d3f3af2256
AnonimPython <moscow.retro@list.ru>
2025-02-14 00:48:01 +0300

    create style for login page

--- Pro_IT_Platform/Pro_IT_Platform.py
+++ Pro_IT_Platform/Pro_IT_Platform.py
@@ -2,12 +2,32 @@ import reflex as rx
 
 from rxconfig import config
 
+#* UI
+from .ui.colors import *
+
+
 #* PAGES
 from .pages.main import main
-from .pages.main import login
+from .pages.login import login
 
 
-app = rx.App()
+app = rx.App(
+    theme=rx.theme(
+        appearance="light",
+        has_background=True,
+    ),
+    #* font
+    stylesheets=[
+        "https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+    ],
+    #* global styles
+    style={
+        "font_family": "Rubik",
+        "background_color": BACKGROUND,
+        "height": "100%",
+        "width": "100%",
+    }
+)
 
 app.add_page(main,route="/",title="Main Page")
 app.add_page(login,route="/login",title="Login Page")
--- Pro_IT_Platform/pages/login.py
+++ Pro_IT_Platform/pages/login.py
@@ -1,5 +1,5 @@
 import reflex as rx
-
+from ..ui.colors import *
 
 class LoginInputState(rx.State):
     form_data: dict = {}
@@ -9,23 +9,93 @@ class LoginInputState(rx.State):
         self.form_data = form_data
         print("Form data:", self.form_data)
 
-def main():
-    return rx.vstack(
-        rx.heading("Login Form"),
-            rx.form.root(
-                rx.hstack(
-                    rx.input(
-                        name="input",
-                        placeholder="Enter text...",
-                        type="text",
-                        required=True,
-                    ),
-                    rx.button("Submit", type="submit"),
+input_style: dict = {
+    "width": "300px",
+    "height": "50px",
+    "--text-field-focus-color": BORDER_INPUT,
+    "background": INPUT_BACKGROUND,
+    "color": "white",
+    "& input::placeholder": {
+        "padding-left":"10px",
+        "color": "white"
+    },
+    "font-size": "20px",
+    }
+
+def login():
+    return rx.center( 
+        rx.hstack(
+            rx.box(
+                rx.image(
+                    "/login_image.jpg",
                     width="100%",
+                    height="500px",
+                    object_fit="cover",
+                    border_radius="10px",
                 ),
-                on_submit=LoginInputState.handle_submit,
-                reset_on_submit=True,
+                width="50%",
+               
+                
             ),
-        
-        rx.button("Click me!", color_scheme="blue"),
+            rx.box(
+                rx.vstack(
+                    rx.heading(
+                        "Pro-IT",
+                        font_size="30px",
+                        width="100%",
+                        color="white",
+                        margin_bottom="50px",
+                        text_align="center",
+                    ),
+                    rx.box(
+                        rx.text("Вход в систему",font_size="20px", color="white"),
+                        #! on relize remove .root
+                        rx.form.root(
+                            rx.vstack(
+                                rx.input(
+                                    name="login",
+                                    placeholder="Логин...",
+                                    type="text",
+                                    required=True,
+                                    style=input_style
+                                    ),
+                                rx.input(
+                                    name="password",
+                                    placeholder="Пароль...",
+                                    type="password",
+                                    required=True,
+                                    style=input_style
+                                ),
+                                rx.button(
+                                    rx.text("Войти", font_size="20px"), 
+                                    type="submit",
+                                    background=BUTTON_BACKGROUND,
+                                    width="300px",
+                                    height="50px",
+                                ),
+                                width="100%",
+                                # gap="30px",
+                            ),
+                            on_submit=LoginInputState.handle_submit,
+                            reset_on_submit=True,
+                        ),                   
+                    ),
+                ),
+                padding="0px 100px 100px 0px",
+                # width="50%",
+                height="auto",
+            ),
+            
+            width="900px",
+            # height="0%",  
+            spacing="8",  
+            padding="15px",
+            border_radius="10px",
+            align_items="center",  
+            background_color=BLOCK_BACKGROUND,
+            color="white",
+            justify="between",
+        ),
+        width="100vw",  
+        height="100vh",
     )
