90c68f11fbc67886afdb3d2c0662b5276da9a85b
AnonimPython <moscow.retro@list.ru>
2025-02-15 02:07:40 +0300

    made a primitive switching between modules in main.py

--- Pro_IT_Platform/pages/main.py
+++ Pro_IT_Platform/pages/main.py
@@ -2,6 +2,14 @@ import reflex as rx
 from ..ui.colors import *
 
 
+class State(rx.State):
+    selected_module: int = 1
+
+    def select_module(self, module: int):
+        """Обработчик выбора модуля."""
+        self.selected_module = module
+
+
 def main():
     return rx.vstack(
         #* header
@@ -42,18 +50,57 @@ def main():
         
         rx.separator(size="4",background=SEPARATOR_COLOR,margin_top="20px"),   
         #* main content
-        rx.box(
-            rx.hstack(
-                #* left side
+        rx.hstack(
+            #* left side
+            rx.box(
+                rx.text("КУРС", font_size="30px"),
+                rx.heading("Scratch", font_size="45px"),
                 rx.box(
-                    rx.text("КУРС"),
-                    rx.text("Scratch"),
+                    rx.text("Модули", font_size="30px"),
+                    rx.box(
+                        rx.vstack(
+                            rx.button("1-Стурктура", 
+                                    on_click=lambda: State.select_module(1)),    
+                            rx.button("2-база", 
+                                    on_click=lambda: State.select_module(2)),    
+                            rx.button("3-учеба", 
+                                    on_click=lambda: State.select_module(3)),    
+                            rx.button("4-перекус", 
+                                    on_click=lambda: State.select_module(4)),    
+                            rx.button("5-выпивка", 
+                                    on_click=lambda: State.select_module(5)),
+                            margin_top="20px",    
+                        ),    
+                    ),
+                    margin_top="50px"    
                 ),
-                #* right side
-                rx.box(rx.text("fewfwe")),
-                width="100%",
+                width="15%",
             ),
+            rx.divider(orientation="vertical", size="4",height="700px",background=SEPARATOR_COLOR),
+            #* right side
+            rx.box(
+                rx.cond(
+                    State.selected_module == 1,
+                    rx.text("Модуль 1 выбран - Структура"),
+                    rx.cond(
+                        State.selected_module == 2,
+                        rx.text("Модуль 2 выбран - База"),
+                        rx.cond(
+                            State.selected_module == 3,
+                            rx.text("Модуль 3 выбран - Учеба"),
+                            rx.cond(
+                                State.selected_module == 4,
+                                rx.text("Модуль 4 выбран - Перекус"),
+                                rx.text("Модуль 5 выбран - Выпивка")
+                            )
+                        )
+                    )
+                ),
+                width="70%",
+            ),
+            spacing="4",
             width="100%",
+            height="10vh"
         ),
         padding="20px",
         color="white",
