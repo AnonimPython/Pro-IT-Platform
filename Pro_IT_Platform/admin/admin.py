dac34ffeec359053f288ed086db10984f0502a0f
AnonimPython <moscow.retro@list.ru>
2025-02-17 23:26:13 +0300

    start working with admin pannel

--- Pro_IT_Platform/admin/admin.py
+++ Pro_IT_Platform/admin/admin.py
@@ -1,11 +1,20 @@
 import reflex as rx
 from .state import AuthState
+from ..ui.colors import *
+
+# rx.button("Выйти", on_click=AuthState.logout), 
 
 def main_content():
-    return rx.vstack(
-        rx.text("Основное окно", size="6", weight="bold"),
-        rx.link("Ссылка 1", href="#"),
-        rx.link("Ссылка 2", href="#"),
-        rx.link("Ссылка 3", href="#"),
-        rx.button("Выйти", on_click=AuthState.logout),  # Кнопка выхода
+    return rx.container(
+        #* header
+        rx.hstack(
+            rx.heading("Pro-IT"),
+            rx.spacer(),
+            rx.button("Выйти", on_click=AuthState.logout),
+        ),
+        rx.hstack(
+            rx.text("Приветствую EVG")    
+        ),
+        background_color=GRAY_LAVANDER,
+        border_radius="10px",
     )
--- Pro_IT_Platform/admin/verify.py
+++ Pro_IT_Platform/admin/verify.py
@@ -1,18 +1,52 @@
 import reflex as rx
 from .state import AuthState
+from ..ui.colors import *
 
+input_style: dict = {
+    "width": "300px",
+    "height": "50px",
+    "--text-field-focus-color": BORDER_INPUT,
+    "background": INPUT_BACKGROUND,
+    "color": "white",
+    "& input::placeholder": {
+        "padding-left":"10px",
+        "color": "white"
+    },
+    "font-size": "20px",
+    }
 def auth_form():
-    return rx.container(
-        rx.text("Google Authenticator", size="6", weight="bold"),
-        rx.form.root(
-            rx.vstack(
-                rx.input(
-                    placeholder="Введите код из Google Authenticator",
-                    value=AuthState.code,
-                    on_change=AuthState.set_code,
+    return rx.center(
+        rx.vstack(
+            rx.text("Введите код из Google Authenticator", size="6", weight="bold",text_align="center",color="white"),
+            #! on relize remove .root
+            rx.form.root(
+                rx.vstack(
+                    rx.input(
+                        placeholder="6-значный код",
+                        value=AuthState.code,
+                        on_change=AuthState.set_code,
+                        style=input_style,
+                        
+                        
+                    ),
+                    rx.button(
+                        "Войти",
+                        type="submit",
+                        width="300px",
+                        background=BLOCK_BACKGROUND,
+                        transition="0.3s",
+                        _hover={
+                            "background": BORDER_INPUT,
+                        }
+                        
+                    ),
+                    
                 ),
-                rx.button("Отправить", type="submit"),
+                on_submit=AuthState.verify_and_redirect,
             ),
-            on_submit=AuthState.verify_and_redirect,
+            width="100%",
+            # spacing="4",
         ),
+        height="100vh",
+        width="100%",
     )
